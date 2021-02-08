6e0fd18517b793b5b6411985dd5a6e5861281233
axelleroi <78707651+axelleroi@users.noreply.github.com>
2021-02-08 11:07:28 +0100

    Update main.js

--- main.js
+++ main.js
@@ -14,35 +14,4 @@ client.on("message", function (message) {
     }
 })
 
-
-
-client.on("message", message => {
-  if (message.content.startsWith("STPplay")) {
-    // On récupère le premier channel audio du serveur
-    let voiceChannel = message.guild.channels
-      .filter(function (channel) { return channel.type === "voice" })
-      .first()
-    // On récupère les arguments de la commande 
-    // il faudrait utiliser une expression régulière pour valider le lien youtube
-    let args = message.content.split(' ')
-    // On rejoint le channel audio
-    voiceChannel
-      .join()
-      .then(function (connection) {
-        // On démarre un stream à partir de la vidéo youtube
-        let stream = YoutubeStream(args[1])
-        stream.on("merde raté", function () {
-          message.reply("Wesh je suis pas jésus , ça marche pas ton truc")
-          connection.disconnect()
-        })
-        // On envoie le stream au channel audio
-        // Il faudrait ici éviter les superpositions (envoie de plusieurs vidéo en même temps)
-        connection
-          .playStream(stream)
-          .on("end", function () {
-            connection.disconnect()
-          })
-      })
-  }
-
-})
+client.login(process.env.TOKEN);
