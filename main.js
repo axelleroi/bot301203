ca9182f044913df7a05beac02710db74ff14c4ac
axelleroi <78707651+axelleroi@users.noreply.github.com>
2021-02-08 11:36:36 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,39 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-
-//Toutes les actions à faire quand le bot se connecte
-client.on("ready", function () {
-    console.log("Mon BOT est Connecté");
-})
-
-// Répondre à un message
-client.on("message", function (message) {
-    if (message.content === "salut") { // Lorsque "Salut" est envoyé
-        message.channel.send("coucou ")
-    }
-})
-
-client.login(process.env.TOKEN);
+var Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./auth.json');
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: auth.TOKEN,
+   autorun: true
+});
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Notre bot doit savoir s’il doit executer une commande
+    // Il sera réceptif en message placés après `!`
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            // !intro
+            case 'intro':
+                bot.sendMessage({
+                    to: channelID,
+                    message: "Bienvenus sur le serveur!"
+                });
+            break;
+            // Ajouter si vous souhaitez d’autres commandes
+         }
+     }
+});
